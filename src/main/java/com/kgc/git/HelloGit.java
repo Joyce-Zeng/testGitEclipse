64bc5a95fd12bb23a59cbe4e352019c8c518c2d9
Joyce-Zeng <1274570081@qq.com>
2020-12-02 11:08:55 +0800

    添加文件

---
+++ src/main/java/com/kgc/git/HelloGit.java
@@ -0,0 +1,20 @@
+package com.kgc.git;
+/**  
+
+* <p>Title: HelloGit.java</p>  
+
+* <p>Description: </p>  
+
+* @author Joyce
+
+* @date 2020-12-2  10:35:29
+
+* @version 1.0  
+
+*/
+
+public class HelloGit {
+ public static void main(String[] args) {
+	System.out.println("Hello Git!");
+}
+}
